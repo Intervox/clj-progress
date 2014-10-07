120095b4381224a579d5356125ca96fe51a9126c
Leonid Beschasny <lbeschastny@iknow.travel>
2014-10-08 02:32:49 +0400

    Refactored test-progress-bar

--- test/clj_progress/bar_test.clj
+++ test/clj_progress/bar_test.clj
@@ -14,71 +14,73 @@
   (are [fmt args res]
     (=  (let [{:keys [tick]} (progress-bar fmt :width 6)]
           (-> args get-state tick with-out-str s/trim))
+        (let [{:keys [done]} (progress-bar fmt :width 6)]
+          (-> args get-state done with-out-str s/trim))
         res)
     ":header"   [ "foo" 42  6   2   7 ] "foo"
-    "[:bar]"    [ "foo" 42  6   2   7 ] "[==>   ]"
-    ":wheel"    [ "foo" 42  6   2   7 ] "|"
     ":done"     [ "foo" 42  6   2   7 ] "2"
     ":total"    [ "foo" 42  6   2   7 ] "6"
     ":elapsed"  [ "foo" 42  6   2   7 ] "42"
-    ":eta"      [ "foo" 42  6   2   7 ] "84"
     ":percent"  [ "foo" 42  6   2   7 ] "33%"
 
-    "> [:bar] :done/:total :elapseds"
-    [ "foo" 42  6   2   7 ]
-    "> [==>   ] 2/6 42s"
-
-    ":header [:wheel] :percent :etas"
-    [ "foo" 42  6   2   7 ]
-    "foo [|] 33% 84s"
-
     ":header"   [ "foo" 42  0   2   7 ] "foo"
-    "[:bar]"    [ "foo" 42  0   2   7 ] "[ ===  ]"
-    ":wheel"    [ "foo" 42  0   2   7 ] "|"
     ":done"     [ "foo" 42  0   2   7 ] "2"
     ":total"    [ "foo" 42  0   2   7 ] "?"
     ":elapsed"  [ "foo" 42  0   2   7 ] "42"
-    ":eta"      [ "foo" 42  0   2   7 ] "?"
     ":percent"  [ "foo" 42  0   2   7 ] "?%"
 
-    "> [:bar] :done/:total :elapseds"
-    [ "foo" 42  0   2   7 ]
-    "> [ ===  ] 2/? 42s"
-
-    ":header [:wheel] :percent :etas"
-    [ "foo" 42  0   2   7 ]
-    "foo [|] ?% ?s"
-
     ":header"   [ "bar" 99  10  5   2 ] "bar"
-    "[:bar]"    [ "bar" 99  10  5   2 ] "[===>  ]"
-    ":wheel"    [ "bar" 99  10  5   2 ] "\\"
     ":done"     [ "bar" 99  10  5   2 ] "5"
     ":total"    [ "bar" 99  10  5   2 ] "10"
     ":elapsed"  [ "bar" 99  10  5   2 ] "99"
-    ":eta"      [ "bar" 99  10  5   2 ] "99"
     ":percent"  [ "bar" 99  10  5   2 ] "50%"
 
-    "> [:bar] :done/:total :elapseds"
-    [ "bar" 99  10  5   2 ]
-    "> [===>  ] 5/10 99s"
-
-    ":header [:wheel] :percent :etas"
-    [ "bar" 99  10  5   2 ]
-    "bar [\\] 50% 99s"
-
     ":header"   [ "bar" 99  -1  5   2 ] "bar"
-    "[:bar]"    [ "bar" 99  -1  5   2 ] "[  === ]"
-    ":wheel"    [ "bar" 99  -1  5   2 ] "\\"
     ":done"     [ "bar" 99  -1  5   2 ] "5"
     ":total"    [ "bar" 99  -1  5   2 ] "?"
     ":elapsed"  [ "bar" 99  -1  5   2 ] "99"
-    ":eta"      [ "bar" 99  -1  5   2 ] "?"
-    ":percent"  [ "bar" 99  -1  5   2 ] "?%"
+    ":percent"  [ "bar" 99  -1  5   2 ] "?%" )
+  (are [fmt args res]
+    (=  (let [{:keys [tick]} (progress-bar fmt :width 6)]
+          (-> args get-state tick with-out-str s/trim))
+        res)
+    "[:bar]"    [ "foo" 42  6   2   7 ] "[==>   ]"
+    ":wheel"    [ "foo" 42  6   2   7 ] "|"
+    ":eta"      [ "foo" 42  6   2   7 ] "84"
 
-    "> [:bar] :done/:total :elapseds"
-    [ "bar" 99  -1  5   2 ]
-    "> [  === ] 5/? 99s"
+    "[:bar]"    [ "foo" 42  0   2   7 ] "[ ===  ]"
+    ":wheel"    [ "foo" 42  0   2   7 ] "|"
+    ":eta"      [ "foo" 42  0   2   7 ] "?"
 
-    ":header [:wheel] :percent :etas"
-    [ "bar" 99  -1  5   2 ]
-    "bar [\\] ?% ?s" ))
+    "[:bar]"    [ "bar" 99  10  5   2 ] "[===>  ]"
+    ":wheel"    [ "bar" 99  10  5   2 ] "\\"
+    ":eta"      [ "bar" 99  10  5   2 ] "99"
+
+    "[:bar]"    [ "bar" 99  -1  5   2 ] "[  === ]"
+    ":wheel"    [ "bar" 99  -1  5   2 ] "\\"
+    ":eta"      [ "bar" 99  -1  5   2 ] "?" )
+  (are [fmt args res]
+    (=  (let [{:keys [done]} (progress-bar fmt :width 6)]
+          (-> args get-state done with-out-str s/trim))
+        res)
+    "[:bar]"    [ "foo" 42  6   2   7 ] "[======]"
+    ":wheel"    [ "foo" 42  6   2   7 ] "+"
+    ":eta"      [ "foo" 42  6   2   7 ] "0"
+
+    "[:bar]"    [ "foo" 42  0   2   7 ] "[======]"
+    ":wheel"    [ "foo" 42  0   2   7 ] "+"
+    ":eta"      [ "foo" 42  0   2   7 ] "0" )
+  (let [fmt   "> [:bar] :percent :done/:total :etas"
+        args  [ "foo" 42 6 2 7 ]]
+    (let [{:keys [tick done]} (progress-bar fmt :width 6)]
+      (is (=  (-> args get-state tick with-out-str s/trim)
+              "> [==>   ] 33% 2/6 84s"))
+      (is (=  (-> args get-state done with-out-str s/trim)
+              "> [======] 33% 2/6 0s"))))
+  (let [fmt   ":header [:wheel] :percent :elapseds"
+        args  [ "bar" 99 -1 5 2 ]]
+    (let [{:keys [tick done]} (progress-bar fmt :width 6)]
+      (is (=  (-> args get-state tick with-out-str s/trim)
+              "> [  === ] ?% 2/? 99s"))
+      (is (=  (-> args get-state done with-out-str s/trim)
+              "> [======] ?% 2/? 99s")))))
